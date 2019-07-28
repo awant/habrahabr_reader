8bf2ef56e5c06600bcc93133804a9e3f2914f6f6
Igor Slobodskov <igor.slobodskov@phystech.edu>
2019-07-28 13:24:46 +0300

    add sbt project

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "habrahabr_reader"
+
+version := "0.1"
+
+scalaVersion := "2.12.8"
---
+++ src/main/scala/com/github/awant/habrareader/HelloWorld.scala
@@ -0,0 +1,6 @@
+package com.github.awant.habrareader
+
+object HelloWorld extends App {
+
+	println("Hello world!")
+}
