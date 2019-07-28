c4aca256d5e42f8c7fa2283c08945d2a6bda904a
Igor Slobodskov <igor.slobodskov@phystech.edu>
2019-07-28 14:19:06 +0300

    add pureconfig which uses typesafe config

--- build.sbt
+++ build.sbt
@@ -4,4 +4,7 @@ version := "0.1"
 
 scalaVersion := "2.12.8"
 
-libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.8" % "test"
+libraryDependencies ++= Seq(
+	"org.scalatest" %% "scalatest" % "3.0.8" % "test",
+	"com.github.pureconfig" %% "pureconfig" % "0.11.1"
+)
--- src/main/scala/com/github/awant/habrareader/HelloWorld.scala
+++ src/main/scala/com/github/awant/habrareader/HelloWorld.scala
@@ -1,8 +1,18 @@
 package com.github.awant.habrareader
 
+import com.typesafe.config.ConfigFactory
+
+import pureconfig.generic.auto._
+
+case class HelloConfig(version: String)
+
 object HelloWorld extends App {
 
 	def helloMessage: String = "Hello world!"
 
 	println(helloMessage)
+
+	//	val cfg: ConfigReader.Result[HelloConfig] = pureconfig.loadConfig[HelloConfig]
+	val helloConfig = pureconfig.loadConfig[HelloConfig](ConfigFactory.load().withFallback(ConfigFactory.load("default.conf")))
+	println(helloConfig.right.get)
 }
