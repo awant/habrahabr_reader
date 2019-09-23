5d86b0c4d666a251f3a61f6e49a48a81336a8f33
Igor Slobodskov <igor.slobodskov@phystech.edu>
2019-09-23 12:01:13 +0300

    simplify encoders, use syntax "key" := value

--- src/main/scala/com/github/awant/habrareader/models/Event.scala
+++ src/main/scala/com/github/awant/habrareader/models/Event.scala
@@ -2,17 +2,13 @@ package com.github.awant.habrareader.models
 
 import java.util.Date
 
-import com.github.awant.habrareader.utils.DateUtils
 import io.circe._
 import io.circe.syntax._
+import com.github.awant.habrareader.utils.DateUtils._
 
 case class Event(id: Long, chatId: Long, messageId: Long, postId: Long, update: Date)
 
 object Event {
-  private implicit val dateEncoder: Encoder[Date] =
-    (date: Date) => DateUtils.convertToStr(date).asJson
-
-  private implicit val dateDecoder: Decoder[Date] = Decoder[String].map(DateUtils.convertToDate(_))
 
   implicit val encoder: Encoder[Event] = (event: Event) => {
     Json.obj(
