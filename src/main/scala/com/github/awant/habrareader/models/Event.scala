24494cb22c250a43bb13c8b16344a7a9b202f264
Igor Slobodskov <igor.slobodskov@phystech.edu>
2019-09-23 11:49:37 +0300

    add encoder and decoder for Date

--- src/main/scala/com/github/awant/habrareader/models/Event.scala
+++ src/main/scala/com/github/awant/habrareader/models/Event.scala
@@ -9,15 +9,22 @@ import io.circe.syntax._
 case class Event(id: Long, chatId: Long, messageId: Long, postId: Long, update: Date)
 
 object Event {
+  private implicit val dateEncoder: Encoder[Date] =
+    (date: Date) => DateUtils.convertToStr(date).asJson
+
+  private implicit val dateDecoder: Decoder[Date] = Decoder[String].map(DateUtils.convertToDate(_))
+
   implicit val encoder: Encoder[Event] = (event: Event) => {
     Json.obj(
       "id" -> event.id.asJson,
       "chatId" -> event.chatId.asJson,
       "messageId" -> event.messageId.asJson,
       "postId" -> event.postId.asJson,
-      "update" -> DateUtils.convertToStr(event.update).asJson
+      "update" -> event.update.asJson,
     )
   }
+
+  implicitly[Decoder[Long]]
 
   implicit val decoder: Decoder[Event] = (c: HCursor) => {
     for {
@@ -25,7 +32,7 @@ object Event {
       chatId <- c.downField("chatId").as[Long]
       messageId <- c.downField("messageId").as[Long]
       postId <- c.downField("postId").as[Long]
-      update <- c.downField("update").as[String]
-    } yield Event(id, chatId, messageId, postId, DateUtils.convertToDate(update))
+      update <- c.downField("update").as[Date]
+    } yield Event(id, chatId, messageId, postId, update)
   }
 }
